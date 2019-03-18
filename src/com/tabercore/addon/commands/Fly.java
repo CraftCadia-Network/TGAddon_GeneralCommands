bed8869d60e9121c09f11924861e39f490f65c54
Sam Taber <admin@cadianetwork.com>
2019-03-17 22:07:50 -0400

    Update Fly.java

--- src/com/tabercore/addon/commands/Fly.java
+++ src/com/tabercore/addon/commands/Fly.java
@@ -1,53 +1,59 @@
 package com.tabercore.addon.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.samtaber.base.Base;
 
 public class Fly implements CommandExecutor{
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player player = (Player) sender;
+		if(label.equalsIgnoreCase("fly")) {
 		
-		if(player.hasPermission("tabercore.fly")) {
-			
-			if(label.equalsIgnoreCase("fly")) {
+			if(player.hasPermission("tabercore.fly")) {
+				if(player.getGameMode() != GameMode.CREATIVE) {
+					
+				player.setAllowFlight(true);
+					
+				}
 				
 				if(args.length == 0) {
-					
-					if(player.isFlying() == false || player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
+					if(player.isFlying()) {
+	
+						player.setFlying(false);
+						player.sendMessage("Set boolean $FLY to FALSE");
+						
+					}else if(!player.isFlying()) {
 						
 						player.setFlying(true);
-						player.sendMessage(Base.globalPrefix + "Set boolean $FLY to TRUE for player " + player.getName());
-						
-					}else if(player.isFlying() == true || player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
-						
-						player.setFlying(false);
-						player.sendMessage(Base.globalPrefix + "Set boolean $FLY to FALSE for player " + player.getName());
+						player.sendMessage("Set boolean $FLY to TRUE");
 						
 					}
-					
 				}else if(args.length == 1) {
 					
 					Player targetPlayer = Bukkit.getServer().getPlayer(args[0]);
 					if(targetPlayer.isFlying() == false) {
 						
 						targetPlayer.setFlying(true);
-						player.sendMessage(Base.globalPrefix + "Set boolean $FLY to TRUE for player " + targetPlayer.getName());
-						targetPlayer.sendMessage(Base.globalPrefix + "Set boolean $FLY to TRUE for player " + targetPlayer.getName());
+						player.sendMessage("Set boolean $FLY to TRUE for player " + targetPlayer.getName());
+						targetPlayer.sendMessage("Set boolean $FLY to TRUE for player " + targetPlayer.getName());
 					}else if(targetPlayer.isFlying() == true) {
 						
 						targetPlayer.setFlying(false);
-						player.sendMessage(Base.globalPrefix + "Set boolean $FLY to FALSE for player " + targetPlayer.getName());
-						targetPlayer.sendMessage(Base.globalPrefix + "Set boolean $FLY to FALSE for player " + targetPlayer.getName());
+						player.sendMessage("Set boolean $FLY to FALSE for player " + targetPlayer.getName());
+						targetPlayer.sendMessage("Set boolean $FLY to FALSE for player " + targetPlayer.getName());
 					}
 				}
+				
+			}else {
+				
+				player.sendMessage(ChatColor.RED + "no perms");
 				
 			}
 			
